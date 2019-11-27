4d8f690461d44422ca0957c9871f41d4aab1393f
Aaron Gooden <aarongooden@Aarons-Mac-Pro.local>
2019-11-27 13:56:46 -0500

    Added routes folder

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("What is up buttercup")
-})
+require('./routes/routes.js')(app)
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
