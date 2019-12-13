cbb616a42686175f04dcdf7a35b0b4a55e8ee9c1
Aaron Gooden <aarongooden@Aarons-Mac-Pro.local>
2019-12-13 13:31:40 -0500

    Adding production capability

--- server/index.js
+++ server/index.js
@@ -3,7 +3,18 @@ const app = express()
 
 require('./routes/routes.js')(app)
 
-app.use(express.static(`${__dirname}/../build`));
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like main.js or main.css file
+    app.use(express.static('client/build'))
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
